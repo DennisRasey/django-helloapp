978c0f0387631ca1e7933580648d0d5e5c5948e6
Dennis Rasey <drasey@babbee.org>
2020-03-14 15:05:49 -0700

    Added 2 simple test for url's '/' & '/about/'. using SimpleTestCase in place of TestCase (I'm a slave to cut&paste).

--- helloapp/howdy/tests.py
+++ helloapp/howdy/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
-
 # Create your tests here.
+
+from django.test import SimpleTestCase
+
+
+class HomePageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+
+class AboutPageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEquals(response.status_code, 200)
